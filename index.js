e5db2ae590654466898b6a62804efdcd4d0521d7
houshengwei <houshengwei@jd.com>
2018-10-21 18:46:22 +0800

    fixed download jd templete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const program = require('commander')
-const download = require('download-git-repo')
 const inquirer = require('inquirer')
 const ora = require('ora')
 const spinner = ora('正在下载模板...')
@@ -8,6 +7,7 @@ const chalk = require('chalk')
 const symbols = require('log-symbols')
 const fs = require('fs')
 const handlebars = require('handlebars')
+const download = require('download')
 
 inquirer.prompt([
   {
@@ -31,63 +31,58 @@ inquirer.prompt([
       name: 'platform',
       message: '这是什么应用?',
       type: 'list',
-      choices: [ "pc", "mobile" ]
+      choices: [ "mobile", "pc" ]
     }
   ]).then(({author, description, platform}) => {
     program.version('1.0.2', "-v, --version")
       .command('init <name>')
       .option('-j, --JDer', '是否下载京东内部模板')
       .action((name, cmd) => {
-        let downloadUrl = cmd.JDer ?
-          'direct:http://minner.jr.jd.com/spe/mpa-templete/dist/mpa.zip' :
-          'imshgga/MPA-for-vue'
+        let url = cmd.JDer ?
+          'http://minner.jr.jd.com/spe/mpa-templete/dist/mpaa.tar' :
+          'https://github.com/imshgga/MPA-for-vue/archive/master.zip'
 
-        if(!fs.existsSync(name)) {
-          spinner.start()
-
-          download(downloadUrl, name, error => {
-            if (error) {
-              spinner.fail()
-              console.log(symbols.error, chalk.red('项目创建失败'))
-              return
+        download(url, name, { extract: true, mode: '666'}).then((data) => {
+          try {
+            {
+              const meta = {
+                name,
+                author,
+                description
+              }
+              const content = fs.readFileSync(`${name}/package.temp.json`).toString()
+              const result = handlebars.compile(content)(meta)
+              fs.writeFileSync(`${name}/package.json`, result)
+              fs.unlink(`${name}/package.temp.json`, error => {
+                if (error) throw error
+              })
             }
 
-            try {
-              {
-                const meta = {
-                  name,
-                  author,
-                  description
-                }
-                const content = fs.readFileSync(`${name}/package.temp.json`).toString()
-                const result = handlebars.compile(content)(meta)
-                fs.writeFileSync(`${name}/package.json`, result)
-              }
+            {
+              fs.renameSync(`${name}/.postcssrc.${platform}.js`, `${name}/.postcssrc.js`)
+              fs.renameSync(`${name}/index.${platform}.html`, `${name}/index.html`)
 
-              {
-                fs.renameSync(`${name}/.postcssrc.${platform}.js`, `${name}/.postcssrc.js`)
-                fs.renameSync(`${name}/index.${platform}.html`, `${name}/index.html`)
-
-                let unlink = platform === 'pc' ? 'mobile' : 'pc'
-                fs.unlink(`${name}/.postcssrc.${unlink}.js`, error => {
-                  if (error) throw error
-                })
-                fs.unlink(`${name}/index.${unlink}.html`, error => {
-                  if (error) throw error
-                })
-              }
-              spinner.succeed()
-              console.log(symbols.success, chalk.green('项目创建成功'))
-              console.log(require('./str.js')(name))
-            } catch (e) {
-              console.log(JSON.stringify(e))
-              spinner.stop()
+              let unlink = platform === 'pc' ? 'mobile' : 'pc'
+              fs.unlink(`${name}/.postcssrc.${unlink}.js`, error => {
+                if (error) throw error
+              })
+              fs.unlink(`${name}/index.${unlink}.html`, error => {
+                if (error) throw error
+              })
             }
-          })
-        } else {
-          console.log(symbols.error, chalk.red('下载模板失败,项目已存在...'))
-        }
 
+            spinner.succeed()
+            console.log(symbols.success, chalk.green('项目创建成功'))
+            console.log(require('./tips.js')(name))
+          } catch (e) {
+            console.log(JSON.stringify(e))
+            spinner.stop()
+          }
+        })
+        .catch(function (err) {
+          console.log(JSON.stringify(err))
+          spinner.stop()
+        })
       })
     program.parse(process.argv)
   })
